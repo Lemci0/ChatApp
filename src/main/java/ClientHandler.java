4d3be8a3c14140fc8ce2ce12b3e07620097195cc
s461995 <kamilberenhard@wp.pl>
2022-01-18 18:10:26 +0100

    creating constructor of ClientHandler class

--- src/main/java/ClientHandler.java
+++ src/main/java/ClientHandler.java
@@ -1,2 +1,33 @@
-public class ClientHandler {
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientHandler implements Runnable {
+
+    public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+    private String clientUsername;
+
+    public ClientHandler(Socket socket) {
+        try {
+            this.socket = socket;
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.clientUsername = bufferedReader.readLine();
+            clientHandlers.add(this);
+            broadcastMessage("SERVER: " + clientUsername + " dolaczyl do rozmowy.");
+        }
+        catch(IOException e) {
+            closeEverything(socket, bufferedWriter, bufferedReader);
+        }
+    }
+
+
+    @Override
+    public void run() {
+        
+
+    }
 }
